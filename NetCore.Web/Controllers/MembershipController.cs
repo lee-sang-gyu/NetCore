61f47424772178aec0f46fba0acc95e3cc06f9f6
DESKTOP-4VRCPN9\LEE <sinlsg0141@gmail.com>
2022-10-07 18:04:15 +0900

    사용자 로그인 정보 입력후 처리를 요청(cshtml) Controller는 ViewModel을 입력받아 로그인 처리한 후 view에 viewModel 전달

--- NetCore.Web/Controllers/MembershipController.cs
+++ NetCore.Web/Controllers/MembershipController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Web.Models;
+using NuGet.Protocol.Plugins;
 
 namespace NetCore.Web.Controllers
 {
@@ -7,6 +9,41 @@ namespace NetCore.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]//ValidateAntiForgeryToken 전부 지정
+        [ValidateAntiForgeryToken] //위조방지토큰을 통해 View로부터 받은 PostData가 유요한지 검증
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+            //view모델 상태 체크
+            if (ModelState.IsValid)
+            {
+                string userId = "ideal1";
+                string password = "123456";
+                if (login.UserId.Equals(userId) && login.Password.Equals(password))
+                {
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+                    return RedirectToAction("Index", "Membership");
+
+                }
+                else 
+                {
+                    message = "로그인되지 않았습니다.";
+                }
+            }
+            else
+            {
+                message = "로그인정보를 올바르게 입력하세요.";
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return View(login);
         }
     }
 }
