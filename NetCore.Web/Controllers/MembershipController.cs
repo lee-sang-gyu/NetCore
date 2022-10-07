e9916f1645e9da5c79ade915f393f15b76763754
DESKTOP-4VRCPN9\LEE <sinlsg0141@gmail.com>
2022-10-07 17:05:22 +0900

    초기 커밋.

---
+++ NetCore.Web/Controllers/MembershipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.Web.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
